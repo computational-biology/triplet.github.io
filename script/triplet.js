93c257a2d09a414f4eb6498ca3aacbcf8961e212
parthajit-roy <roy.parthajit@gmail.com>
2022-10-28 20:07:36 +0530

    page population logic updated

--- script/triplet.js
+++ script/triplet.js
@@ -20,23 +20,52 @@ function isUpperCase(a){
    }
 }
 
-function get_bptype(bp, rescls1, rescls2){
+function get_bptype_std(bp, rescls1, rescls2){
       if(isUpperCase(rescls1) === false || isUpperCase(rescls2) === false){
-          return "MOD";
+          return "NO-MATCH";
       }
       var a = bp.substr(0,1);
       var b = bp.substr(2,1);
       if((a === "W" || a === "H" || a === "S") && (b === "W" || b === "H" || b === "S")){
 	    return "STD";
-      }else if((a === "+" || a === "z" || a === "g") || (b === "+" || b === "g" || b ==="z")){
-        return "PROTO";
-      }else if(isUpperCase(bp.substr(3,1)) === false){
-          return "SUG";
-      }else{
-          return "CHO";
+      }else {
+	    return "NO-MATCH";
       }
 }
 
+function get_bptype_cho(bp, rescls1, rescls2){
+      if(isUpperCase(bp.substr(0,1)) === false){
+          return "CHO";
+      }else{
+          return "NO-MATCH";
+      }
+}
+
+function get_bptype_sug(bp, rescls1, rescls2){
+      if(isUpperCase(bp.substr(3,1)) === false){
+          return "SUG";
+      }else{
+          return "NO-MATCH";
+      }
+}
+
+function get_bptype_mod(bp, rescls1, rescls2){
+      if(isUpperCase(rescls1) === false || isUpperCase(rescls2) == false){
+          return "MOD";
+      }else{
+          return "NO-MATCH";
+      }
+}
+
+function get_bptype_proto(bp, rescls1, rescls2){
+      var a = bp.substr(0,1);
+      var b = bp.substr(2,1);
+      if((a === "+" || a === "z" || a === "g") || (b === "+" || b === "g" || b ==="z")){
+        return "PROTO";
+      }else{
+	    return "NO-MATCH";
+      }
+}
 
 function is_std_basepair(x){
       
@@ -69,7 +98,7 @@ function show_details(accn, i){
       }
       
       
-function show_trip_details_others(bptype){
+function show_trip_details_proto(bptype){
 
       var accnval = document.getElementById("accn").value;
       var tripnameval = document.getElementById("tripname").value;
@@ -95,7 +124,163 @@ function show_trip_details_others(bptype){
       var str = "<table id=\"resulttable\"><tr><td>ACCN</td><td>TRP</td><td>EDG1-EDG2</td><td>  View in JsMol</td><td>Detail Only</td></tr>";
         var count = 0;
          for(var i=0; i<tripdata.length; ++i){
-	    if(tripdata[i].compname === "T1" && (get_bptype(tripdata[i].bpname1, tripdata[i].rescls1, tripdata[i].rescls2) === bptype || get_bptype(tripdata[i].bpname23, tripdata[i].rescls2, tripdata[i].rescls3) === bptype)){
+	    if(tripdata[i].compname === "T1" && (get_bptype_proto(tripdata[i].bpname1, tripdata[i].rescls1, tripdata[i].rescls2) === bptype || get_bptype_proto(tripdata[i].bpname23, tripdata[i].rescls2, tripdata[i].rescls3) === bptype)){
+		  if((accnval.length === 0 || tripdata[i].accn === accnval.toLowerCase())  
+			&& 
+			(tripnameval.length === 0 || tripdata[i].tripname === tripnameval)
+			&&
+			(edge1 === "ALL" || tripdata[i].bpname1.substr(0,3) === edge1)
+			&&
+			(orival1 === "all"|| orival1 === tripdata[i].bpname1.substr(3,1))
+			&&
+			(edge2 === "ALL" || tripdata[i].bpname23.substr(0,3) === edge2)
+			&&
+			(orival2 === "all"|| orival2 === tripdata[i].bpname23.substr(3,1))
+		  ){
+			//str = str + "<tr><td>"+tripdata[i].accn + "</td><td>" + tripdata[i].tripname + " </td><td>  " + tripdata[i].edgeinfo + " </td><td> [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', ' select " +tripdata[i].resid1+":"+tripdata[i].chain1+","+tripdata[i].resid2+":"+tripdata[i].chain2+","+tripdata[i].resid3+":"+tripdata[i].chain3+"' );\">Full</a>]       [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', 'restrict within(15.0, " +tripdata[i].resid1+":"+tripdata[i].chain1+ "); select " +tripdata[i].resid1+":"+tripdata[i].chain1+","+tripdata[i].resid2+":"+tripdata[i].chain2+","+tripdata[i].resid3+":"+tripdata[i].chain3+"' );\">Within 15A</a>]" + "</td><td> <a href=\"javascript:void(0);\" onclick=\"modal_disp("+i+");\">Show details</a> </td></tr>";
+
+             str = str + "<tr><td>"+tripdata[i].accn + "</td><td>" + tripdata[i].tripname + " </td><td>  " + tripdata[i].edgeinfo + " </td><td> [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', "+ i +", 'FULL');\">Full</a>]       [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', "+ i +", 'WITHIN15A');\">Within 15A</a>]" + "  [<a href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', "+ i +", 'TRIPLETONLY');\">Triplet Only</a>]</td> <td><a  href=\"javascript:void(0);\" onclick=\"show_details('" + tripdata[i].accn + "', "+ i +");\">Details</a></tr>";
+		     count = count + 1;
+		  
+		  }
+
+	    }
+      }
+      str = str + "</table>";
+      str = "<h3>Total Cases Found:"+count+"</h3>" + str;
+      document.getElementById("jsontest").innerHTML = str;
+}
+function show_trip_details_sug(bptype){
+
+      var accnval = document.getElementById("accn").value;
+      var tripnameval = document.getElementById("tripname").value;
+      var edge1 = document.getElementById("edge1").value;
+      var ori1 = document.getElementsByName("orient1");
+      var orival1;
+      for(var i = 0; i < ori1.length; i++){
+	    if(ori1[i].checked === true){
+		  orival1 = ori1[i].value;
+	    }
+      }
+
+      var edge2 = document.getElementById("edge2").value;
+      var ori2 = document.getElementsByName("orient2");
+      var orival2;
+      for(var i = 0; i < ori2.length; i++){
+	    if(ori2[i].checked === true){
+		  orival2 = ori2[i].value;
+	    }
+      }
+//      window.alert(orival2);
+//      var ori2 = document.getElementByName("orient2").value;
+      var str = "<table id=\"resulttable\"><tr><td>ACCN</td><td>TRP</td><td>EDG1-EDG2</td><td>  View in JsMol</td><td>Detail Only</td></tr>";
+        var count = 0;
+         for(var i=0; i<tripdata.length; ++i){
+	    if(tripdata[i].compname === "T1" && (get_bptype_sug(tripdata[i].bpname1, tripdata[i].rescls1, tripdata[i].rescls2) === bptype || get_bptype_sug(tripdata[i].bpname23, tripdata[i].rescls2, tripdata[i].rescls3) === bptype)){
+		  if((accnval.length === 0 || tripdata[i].accn === accnval.toLowerCase())  
+			&& 
+			(tripnameval.length === 0 || tripdata[i].tripname === tripnameval)
+			&&
+			(edge1 === "ALL" || tripdata[i].bpname1.substr(0,3) === edge1)
+			&&
+			(orival1 === "all"|| orival1 === tripdata[i].bpname1.substr(3,1))
+			&&
+			(edge2 === "ALL" || tripdata[i].bpname23.substr(0,3) === edge2)
+			&&
+			(orival2 === "all"|| orival2 === tripdata[i].bpname23.substr(3,1))
+		  ){
+			//str = str + "<tr><td>"+tripdata[i].accn + "</td><td>" + tripdata[i].tripname + " </td><td>  " + tripdata[i].edgeinfo + " </td><td> [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', ' select " +tripdata[i].resid1+":"+tripdata[i].chain1+","+tripdata[i].resid2+":"+tripdata[i].chain2+","+tripdata[i].resid3+":"+tripdata[i].chain3+"' );\">Full</a>]       [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', 'restrict within(15.0, " +tripdata[i].resid1+":"+tripdata[i].chain1+ "); select " +tripdata[i].resid1+":"+tripdata[i].chain1+","+tripdata[i].resid2+":"+tripdata[i].chain2+","+tripdata[i].resid3+":"+tripdata[i].chain3+"' );\">Within 15A</a>]" + "</td><td> <a href=\"javascript:void(0);\" onclick=\"modal_disp("+i+");\">Show details</a> </td></tr>";
+
+             str = str + "<tr><td>"+tripdata[i].accn + "</td><td>" + tripdata[i].tripname + " </td><td>  " + tripdata[i].edgeinfo + " </td><td> [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', "+ i +", 'FULL');\">Full</a>]       [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', "+ i +", 'WITHIN15A');\">Within 15A</a>]" + "  [<a href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', "+ i +", 'TRIPLETONLY');\">Triplet Only</a>]</td> <td><a  href=\"javascript:void(0);\" onclick=\"show_details('" + tripdata[i].accn + "', "+ i +");\">Details</a></tr>";
+		     count = count + 1;
+		  
+		  }
+
+	    }
+      }
+      str = str + "</table>";
+      str = "<h3>Total Cases Found:"+count+"</h3>" + str;
+      document.getElementById("jsontest").innerHTML = str;
+}
+function show_trip_details_cho(bptype){
+
+      var accnval = document.getElementById("accn").value;
+      var tripnameval = document.getElementById("tripname").value;
+      var edge1 = document.getElementById("edge1").value;
+      var ori1 = document.getElementsByName("orient1");
+      var orival1;
+      for(var i = 0; i < ori1.length; i++){
+	    if(ori1[i].checked === true){
+		  orival1 = ori1[i].value;
+	    }
+      }
+
+      var edge2 = document.getElementById("edge2").value;
+      var ori2 = document.getElementsByName("orient2");
+      var orival2;
+      for(var i = 0; i < ori2.length; i++){
+	    if(ori2[i].checked === true){
+		  orival2 = ori2[i].value;
+	    }
+      }
+//      window.alert(orival2);
+//      var ori2 = document.getElementByName("orient2").value;
+      var str = "<table id=\"resulttable\"><tr><td>ACCN</td><td>TRP</td><td>EDG1-EDG2</td><td>  View in JsMol</td><td>Detail Only</td></tr>";
+        var count = 0;
+         for(var i=0; i<tripdata.length; ++i){
+	    if(tripdata[i].compname === "T1" && (get_bptype_cho(tripdata[i].bpname1, tripdata[i].rescls1, tripdata[i].rescls2) === bptype || get_bptype_cho(tripdata[i].bpname23, tripdata[i].rescls2, tripdata[i].rescls3) === bptype)){
+		  if((accnval.length === 0 || tripdata[i].accn === accnval.toLowerCase())  
+			&& 
+			(tripnameval.length === 0 || tripdata[i].tripname === tripnameval)
+			&&
+			(edge1 === "ALL" || tripdata[i].bpname1.substr(0,3) === edge1)
+			&&
+			(orival1 === "all"|| orival1 === tripdata[i].bpname1.substr(3,1))
+			&&
+			(edge2 === "ALL" || tripdata[i].bpname23.substr(0,3) === edge2)
+			&&
+			(orival2 === "all"|| orival2 === tripdata[i].bpname23.substr(3,1))
+		  ){
+			//str = str + "<tr><td>"+tripdata[i].accn + "</td><td>" + tripdata[i].tripname + " </td><td>  " + tripdata[i].edgeinfo + " </td><td> [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', ' select " +tripdata[i].resid1+":"+tripdata[i].chain1+","+tripdata[i].resid2+":"+tripdata[i].chain2+","+tripdata[i].resid3+":"+tripdata[i].chain3+"' );\">Full</a>]       [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', 'restrict within(15.0, " +tripdata[i].resid1+":"+tripdata[i].chain1+ "); select " +tripdata[i].resid1+":"+tripdata[i].chain1+","+tripdata[i].resid2+":"+tripdata[i].chain2+","+tripdata[i].resid3+":"+tripdata[i].chain3+"' );\">Within 15A</a>]" + "</td><td> <a href=\"javascript:void(0);\" onclick=\"modal_disp("+i+");\">Show details</a> </td></tr>";
+
+             str = str + "<tr><td>"+tripdata[i].accn + "</td><td>" + tripdata[i].tripname + " </td><td>  " + tripdata[i].edgeinfo + " </td><td> [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', "+ i +", 'FULL');\">Full</a>]       [<a  href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', "+ i +", 'WITHIN15A');\">Within 15A</a>]" + "  [<a href=\"javascript:void(0);\" onclick=\"runjsmol('" + tripdata[i].accn + "', "+ i +", 'TRIPLETONLY');\">Triplet Only</a>]</td> <td><a  href=\"javascript:void(0);\" onclick=\"show_details('" + tripdata[i].accn + "', "+ i +");\">Details</a></tr>";
+		     count = count + 1;
+		  
+		  }
+
+	    }
+      }
+      str = str + "</table>";
+      str = "<h3>Total Cases Found:"+count+"</h3>" + str;
+      document.getElementById("jsontest").innerHTML = str;
+}
+function show_trip_details_mod(bptype){
+
+      var accnval = document.getElementById("accn").value;
+      var tripnameval = document.getElementById("tripname").value;
+      var edge1 = document.getElementById("edge1").value;
+      var ori1 = document.getElementsByName("orient1");
+      var orival1;
+      for(var i = 0; i < ori1.length; i++){
+	    if(ori1[i].checked === true){
+		  orival1 = ori1[i].value;
+	    }
+      }
+
+      var edge2 = document.getElementById("edge2").value;
+      var ori2 = document.getElementsByName("orient2");
+      var orival2;
+      for(var i = 0; i < ori2.length; i++){
+	    if(ori2[i].checked === true){
+		  orival2 = ori2[i].value;
+	    }
+      }
+//      window.alert(orival2);
+//      var ori2 = document.getElementByName("orient2").value;
+      var str = "<table id=\"resulttable\"><tr><td>ACCN</td><td>TRP</td><td>EDG1-EDG2</td><td>  View in JsMol</td><td>Detail Only</td></tr>";
+        var count = 0;
+         for(var i=0; i<tripdata.length; ++i){
+	    if(tripdata[i].compname === "T1" && (get_bptype_mod(tripdata[i].bpname1, tripdata[i].rescls1, tripdata[i].rescls2) === bptype || get_bptype_mod(tripdata[i].bpname23, tripdata[i].rescls2, tripdata[i].rescls3) === bptype)){
 		  if((accnval.length === 0 || tripdata[i].accn === accnval.toLowerCase())  
 			&& 
 			(tripnameval.length === 0 || tripdata[i].tripname === tripnameval)
@@ -204,7 +389,7 @@ function show_trip_details_std(bptype){
       var str = "<table id=\"resulttable\"><tr><td>ACCN</td><td>TRP</td><td>EDG1-EDG2</td><td>  View in JsMol</td><td>Detail Only</td></tr>";
         var count = 0;
       for(var i=0; i<tripdata.length; ++i){
-	    if(tripdata[i].compname === "T1" && get_bptype(tripdata[i].bpname1, tripdata[i].rescls1, tripdata[i].rescls2) === bptype && get_bptype(tripdata[i].bpname23, tripdata[i].rescls2, tripdata[i].rescls3) === bptype){
+	    if(tripdata[i].compname === "T1" && get_bptype_std(tripdata[i].bpname1, tripdata[i].rescls1, tripdata[i].rescls2) === bptype && get_bptype_std(tripdata[i].bpname23, tripdata[i].rescls2, tripdata[i].rescls3) === bptype){
 		  if((accnval.length === 0 || tripdata[i].accn === accnval.toLowerCase())  
 			&& 
 			(tripnameval.length === 0 || tripdata[i].tripname === tripnameval)
